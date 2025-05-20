ff7d9f02e20fea3ad98c7662a477d40ce760e77d
sora <ksora093@gmail.com>
2025-05-21 04:24:06 +0900

    add searchBox

---
+++ src/app/(public)/page.tsx
@@ -0,0 +1,26 @@
+import PostCard from "@/components/post/PostCard";
+import { getPosts, searchPosts } from "@/lib/post";
+import { Post } from "@/types/post";
+
+type SearchParams = {
+  searchParams: Promise<{ search: string }>;
+};
+
+export default async function PostsPage({ searchParams }: SearchParams) {
+  const resolvedSearchParams = await searchParams;
+  const query = resolvedSearchParams.search;
+
+  const posts = query
+    ? ((await searchPosts(query)) as Post[])
+    : ((await getPosts()) as Post[]);
+
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {posts.map((post) => (
+          <PostCard key={post.id} post={post} />
+        ))}
+      </div>
+    </div>
+  );
+}
---
+++ src/components/post/PostCard.tsx
@@ -0,0 +1,46 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { PostCardProps } from "@/types/post";
+import Link from "next/link";
+import { formatDistanceToNow } from "date-fns";
+import { ja } from "date-fns/locale";
+import Image from "next/image";
+
+export default function PostCard({ post }: PostCardProps) {
+  return (
+    <Card className="hover:shadow-lg transition-shadow pt-0 gap-0">
+      <Link href={`/posts/${post.id}`}>
+        {post.topImage && (
+          <div className="relative w-full h-48">
+            <Image
+              src={post.topImage}
+              alt={post.title}
+              fill
+              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+              className="rounded-t-md object-cover"
+              priority
+            />
+          </div>
+        )}
+        <CardHeader>
+          <CardTitle className="line-clamp-2 py-4 text-lg">
+            {post.title}
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <p className="text-sm text-gray-600 mb-2 line-clamp-2">
+            {post.content}
+          </p>
+          <div className="flex items-center justify-between text-sm text-gray-500">
+            <span>投稿者: {post.author.name}</span>
+            <time>
+              {formatDistanceToNow(new Date(post.createdAt), {
+                addSuffix: true,
+                locale: ja,
+              })}
+            </time>
+          </div>
+        </CardContent>
+      </Link>
+    </Card>
+  );
+}
