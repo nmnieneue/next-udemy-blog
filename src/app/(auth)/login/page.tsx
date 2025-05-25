1a281486e8b7fb6b68c7d97de480dce5d3771c2c
sora <ksora093@gmail.com>
2025-05-25 18:06:18 +0900

    add logo
    ログインフォームと登録フォーム画面にロゴ(トップページへのリンク)を追加

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,5 +1,14 @@
 import LoginForm from "@/components/auth/LoginForm";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function LoginPage() {
-  return <LoginForm />;
+  return (
+    <div className="container flex flex-col items-center mx-auto space-y-6">
+      <Link href="/" className="hover:bg-white focus:bg-white mr-1">
+        <Image src="/image.jpeg" alt="logo" width={150} height={0} />
+      </Link>
+      <LoginForm />
+    </div>
+  );
 }
--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,6 +1,15 @@
 import RegisterForm from "@/components/auth/RegisterForm";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 export default function RagisterPage() {
-  return <RegisterForm />;
+  return (
+    <div className="container flex flex-col items-center mx-auto space-y-6">
+      <Link href="/" className="hover:bg-white focus:bg-white mr-1">
+        <Image src="/image.jpeg" alt="logo" width={150} height={0} />
+      </Link>
+      <RegisterForm />
+    </div>
+  );
 }
